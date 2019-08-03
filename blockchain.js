274e4ba8506f47bd12b34ed4a5272556b1308844
Tayte Stokes <taytestokes9328@gmail.com>
2019-08-03 12:24:59 -0600

    feat: added prototype method createNewBlock to Blockchain

--- blockchain.js
+++ blockchain.js
@@ -1,6 +1,31 @@
-// block chain data structure
+/* Blockchain Data Structure */
 function Blockchain(){
     this.chain = [];
     this.newTransactions = [];
 };
 
+/* Blockchain Prototype Methods */
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
+    // create a new block on the chain
+    const newBlock = {
+        index: this.chain.length + 1,
+        timeStamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce,
+        previousBlockHash,
+        hash
+    };
+
+    // clear transactions array
+    this.newTransactions = [];
+
+    // add the new block to the chain
+    this.chain.push(newBlock);
+
+    // return the new block
+    return newBlock;
+};
+
+
+/* Export Blockchain Data Structure */
+module.exports = Blockchain;
